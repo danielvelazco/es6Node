2311c713e6212a6144034aae8acdb6f0fde7293c
Daniel Velazco <daniel.velazco@globant.com>
2020-03-02 12:17:31 -0500

    es6 with node

---
+++ src/app.mjs
@@ -0,0 +1,12 @@
+//import request from 'requests';
+
+//require("@babel/core").transform("code", options);
+
+let helloWorld = `Hello World!`;
+console.log(`${helloWorld} this is some ES6 JavaScript code`);
+
+import Car from './classes/Car.mjs'
+
+let car = new Car();
+
+console.log(car.getDoors());
---
+++ src/classes/Car.mjs
@@ -0,0 +1,14 @@
+export default class Car {
+    constructor(){
+        this.doors = 4;
+        this.wheels = 4;
+    }
+
+    getDoors() {
+        return this.doors;
+    }
+
+    getWheels() {
+        return this.wheels;
+    }
+}
